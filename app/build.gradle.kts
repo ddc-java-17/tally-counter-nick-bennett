56c1ff284bd12a3f8b81c4e15caf08b539a67c20
nick-bennett <nbennett10@cnm.edu>
2024-02-05 14:57:43 -0700

    Added view binding.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,10 +26,16 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
+
+    buildFeatures {
+        viewBinding = true
+    }
+
 }
 
 dependencies {
